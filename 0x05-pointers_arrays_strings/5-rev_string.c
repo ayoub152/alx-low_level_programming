7d268022fbdc7cc853038627c95161fb67f918c3
ayoub152 <ahmedbouallaga123@gmail.com>
2023-03-27 15:27:18 -0700

    khalass

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,11 +4,16 @@
  * @s: Input string
  * Return: String in reverse
  */
+
 void rev_string(char *s)
+
 {
 	char rev = s[0];
+
 	int counter = 0;
+
 	int i;
+
 	while (s[counter] != '\0')
 		counter++;
 	for (i = 0; i < counter; i++)
